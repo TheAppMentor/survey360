5a5afd54d52fb5efcce520cc868303834e0502ff
Raghu, Reshma L <reshma.raghu@sap.com>
2019-04-12 14:17:43 +0530

    adding api for creatimg users

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var DatabaseHandler = require('../database/MongoDBHandler').MongoDBHandler
-//import { DatabaseHandler } from '../database/MongoDBHandler';
-
-const dbHandler = new DatabaseHandler()
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/addUser', function(req, res, next) {
-    dbHandler.insertUser({name: "Boy 1", email : "boy1@boysclub.com"})
-    res.render('index', { title: 'Add User' });
-
-});
-
-router.get('/listUsers', function(req, res, next) {
-    //res.render('index', { title: 'User List' });
-    dbHandler.listAllUsers()
-        .then((allData) => {
-           console.log("RESOLVING WITH Results..  : " + allData) 
-            res.json(allData)
-        })
+  res.render('index', 
+  	{ 
+  		title: 'Survey360' 
+  	});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser')
+// create application/json parser
+var jsonParser = bodyParser.json()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const DatabaseHandler = require('../database/MongoDBHandler').MongoDBHandler
+const dbHandler = new DatabaseHandler()
+
+/* POST */
+router.post('/createUsers', jsonParser, function(req, res, next) {
+
+	const arrayOfUsers = req.body;
+	arrayOfUsers.forEach((user) => {
+		const userName = user.name;
+		const emailId = user.email;
+		dbHandler.insertUser({name: userName, email : emailId})
+		console.log("CREATING User  : " + userName) 
+	});
+  	
+  	dbHandler.listAllUsers()
+        .then((allData) => {
+           console.log("RESOLVING WITH Results..  : " + allData) 
+            res.json(allData)
+        })
+});
+
+/* GET */
+router.get('/readUsers', function(req, res, next) {
+    dbHandler.listAllUsers()
+        .then((allData) => {
+           	console.log("RESOLVING WITH Results..  : " + allData) 
+            res.json(allData)
+        })
+});
+
+/* PUT */
+router.put('/updateUsers', function(req, res, next) {
+   
+});
+
+/* DELETE */
+router.delete('/deleteUsers', function(req, res, next) {
+    
 });
 
 module.exports = router;
